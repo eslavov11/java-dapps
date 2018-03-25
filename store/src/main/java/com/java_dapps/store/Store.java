0ee4ce95142f02c120d6ded97d6bba984135119d
Edward <e.slavov11@gmail.com>
2018-03-25 13:50:21 +0300

    init store spring project

---
+++ store/src/main/java/com/java_dapps/store/Store.java
@@ -0,0 +1,11 @@
+package com.java_dapps.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Store {
+    public static void main(String[] args) {
+        SpringApplication.run(Store.class, args);
+    }
+}
